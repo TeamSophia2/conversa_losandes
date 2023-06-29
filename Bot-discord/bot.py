4a54a0e37bc5d2630dc711c20742ba8f913958a9
fernando479 <fernando.ruiz.l@hotmail.cl>
2023-06-29 19:51:08 -0400

    delete .env

--- Bot-discord/bot.py
+++ Bot-discord/bot.py
@@ -1,11 +1,19 @@
 import os
 import random
+import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
 
+
+load_dotenv()
+#TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = 'MTA5MDk4NTc4NjMzNTMwNTgyMA.GdFag7.c2dkiD0zZt8Qt2MWHN9Q4sTi4G0bH4VtsmoWyg'
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
 bot = commands.Bot(command_prefix='!')
 
 @bot.command(name='roll', help='Simula el lanzamiento de un dado. Usa !roll numdados numlados')
