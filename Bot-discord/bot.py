830edee96fe720b88e0bd8e0cfe2d8cff50fd365
BothEyes <a.navarrogg@gmail.com>
2023-07-18 11:08:02 -0400

    test pdf

--- Bot-discord/bot.py
+++ Bot-discord/bot.py
@@ -39,23 +39,28 @@ class BOT(commands.Cog):
     
     
     @commands.command(name='addPdf')
-    async def addDocument(self, ctx):
+    async def addPdf(self, ctx):
         message = ctx.message
         # Verifica si se adjuntó un archivo al mensaje
         if len(message.attachments) > 0:
-            # Lee el archivo PDF desde el sistema de archivos en modo binario ('rb')
-            with open('ruta_del_archivo.pdf', 'rb') as pdf_file:
-                pdf_data = pdf_file.read()
+            file = message.attachments[0]
+            # Verifica si el archivo es un PDF
+            if file.filename.lower().endswith('.pdf'):
+                # Obtiene los datos binarios del archivo PDF adjunto
+                pdf_data = await file.read()
 
-            # Llama a la función readPdf con los datos del archivo PDF
-            tools = Tools()
-            df = tools.readPdf(pdf_data)
-            
-            # Verifica si se ha obtenido el DataFrame con el texto y el lenguaje
-            if df is not None:
-                print(df)
+                # Lee y valida el archivo PDF
+                tools = Tools()
+                df = tools.readPdf(pdf_data)
+
+                if df is not None:
+                    await ctx.send('El archivo PDF ha sido procesado correctamente.')
+                    # Aquí puedes trabajar con el DataFrame 'df' que contiene el texto y el lenguaje.
+                else:
+                    await ctx.send('Ha ocurrido un error al procesar el archivo PDF.')
+
             else:
-                print("Ha ocurrido un error al procesar el archivo PDF.")
+                await ctx.send('El archivo adjunto no es un PDF válido.')
         else:
             await ctx.send('No se ha adjuntado ningún archivo al mensaje.')
 
