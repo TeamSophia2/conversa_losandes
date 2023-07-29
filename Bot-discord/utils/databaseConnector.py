7403263520d73eb27ee6e99a321e1aa6a9d23935
fernando479 <fernando.ruiz.l@hotmail.cl>
2023-07-28 20:08:20 -0400

    complete organization table and avoid duplicate documents

--- Bot-discord/utils/databaseConnector.py
+++ Bot-discord/utils/databaseConnector.py
@@ -48,6 +48,7 @@ class DatabaseConnector:
             abstract = None
             category = row['CATEGORÍA']
             author = row['AUTORES/AS']
+            organization = row['REVISTA']
 
             # Verifica si el valor doi es NaN y reemplázalo con None
             if pd.notna(doi):
@@ -66,106 +67,146 @@ class DatabaseConnector:
                 content,
                 abstract
             )
-            cursor.execute(query, values)
-            self.connection.commit()
 
-            # Obtén el documentId recién insertado
-            documentId = cursor.lastrowid
+            # consulta para verificar si el título del documento ya existe en la base de datos.
+            cursor.execute(
+                "SELECT COUNT(*) FROM Document WHERE title = %s", (title,))
 
-            # ingresa datos a Document_Category
-            category_list = category.split(';')
-            for category in category_list:
-                category = category.strip()
-                # Verifica que la temática exista en la tabla Tematica
-                cursor.execute(
-                    "SELECT categoryName FROM Category WHERE categoryName = %s", (category,))
-                result = cursor.fetchone()
+            if cursor.fetchone()[0] == 0:
+                cursor.execute(query, values)
+                self.connection.commit()
 
-                if result:
-                    # Inserta la relación en Documento_Tematica
+                # Obtener  el documentId recién insertado
+                documentId = cursor.lastrowid
+
+                # ingresar datos a Document_Category
+                category_list = category.split(';')
+                for category in category_list:
+                    category = category.strip()
+                    # Verificar que la temática exista en la tabla Tematica
                     cursor.execute(
-                        "INSERT INTO Document_Category (documentId, categoryName) VALUES (%s, %s)", (documentId, category))
-                    self.connection.commit()
-                else:
-                    # Si la temática no existe, manejar el caso
-                    print(w)
+                        "SELECT categoryName FROM Category WHERE categoryName = %s", (category,))
+                    result = cursor.fetchone()
 
-            # Insertar datos en tabla Author
-            authors = [author.strip() for author in author.split(';')]
-            for author in authors:
-                # Verifica si el autor ya existe en la tabla Author
-                query = "SELECT authorId FROM Author WHERE authorName = %s"
+                    if result:
+                        # Insertar la relación en Documento_Tematica
+                        cursor.execute(
+                            "INSERT INTO Document_Category (documentId, categoryName) VALUES (%s, %s)", (documentId, category))
+                        self.connection.commit()
+                    else:
+                        # Si la temática no existe, manejar el caso
+                        print(w)
+
+                # Insertar datos en tabla Author
+                authors = [author.strip() for author in author.split(';')]
+                for author in authors:
+                    # Verifica si el autor ya existe en la tabla Author
+                    query = "SELECT authorId FROM Author WHERE authorName = %s"
+                    # Agrega strip() para eliminar espacios en blanco al inicio y final del nombre
+                    values = (author,)
+                    cursor.execute(query, values)
+                    result = cursor.fetchone()
+
+                    if result:
+                        # Si el autor ya existe, no es necesario insertarlo nuevamente
+                        # print(f"El autor '{author}' ya está registrado en la base de datos.")
+                        authorId = result[0]
+                    else:
+                        # Si el autor no existe, insertarlo en la tabla Author
+                        query = "INSERT INTO Author (authorName) VALUES (%s)"
+                        cursor.execute(query, values)
+                        self.connection.commit()
+
+                        cursor.execute(
+                            "SELECT authorId FROM Author WHERE authorName = %s", (author,))
+                        authorId = cursor.fetchone()[0]
+
+                    # Inserta la relación en Document_Author
+                    cursor.execute(
+                        "INSERT INTO Document_Author (documentId, authorId) VALUES (%s, %s)", (documentId, authorId))
+                    self.connection.commit()
+
+                # Insertar datos en tabla Commune
+                comunas = [str(c).strip() for c in str(row['COMUNAS']).split(
+                    ',') if str(c).strip() and str(c).strip().lower() != 'nan']
+                # Verificar si las comunas ya existen en la tabla Commune
+                for comuna in comunas:
+                    cursor.execute(
+                        "SELECT communeId FROM Commune WHERE name = %s ", (comuna, ))
+                    result = cursor.fetchone()
+
+                    if result:
+                        communeId = result[0]
+                        # Si la comuna ya existe, no es necesario agregarla nuevamente
+
+                    else:
+                        # Buscar la región correspondiente en el DataFrame commune_region_df
+                        regionMatch = communeRegionData[communeRegionData['Comuna'] == comuna]
+
+                        # match
+                        if not regionMatch.empty:
+                            region = regionMatch['Región'].iloc[0]
+                            latitude = regionMatch['Latitud (Decimal)'].iloc[0]
+                            longitude = regionMatch['Longitud (decimal)'].iloc[0]
+
+                            query = "INSERT INTO Commune (name, region, latitude, longitude) VALUES (%s, %s, %s, %s)"
+                            values = (comuna, region, latitude, longitude)
+                            cursor.execute(query, values)
+                            self.connection.commit()
+
+                            cursor.execute(
+                                "SELECT communeId FROM Commune WHERE name = %s", (comuna,))
+                            communeId = cursor.fetchone()[0]
+
+                        # no match
+                        else:
+                            # Insertar la comuna en la tabla Commune con los campos region, latitude y longitude como NULL
+                            # print(f"No se encontró la región para la comuna '{comuna}'. Se debe realizar el match manualmente.")
+                            query = "INSERT INTO Commune (name, region, latitude, longitude) VALUES (%s, NULL, NULL, NULL)"
+                            values = (comuna,)
+                            cursor.execute(query, values)
+                            self.connection.commit()
+                            cursor.execute(
+                                "SELECT communeId FROM Commune WHERE name = %s", (comuna,))
+                            communeId = cursor.fetchone()[0]
+
+                        # Inserta la relación en Document_Commune
+                    cursor.execute(
+                        "INSERT INTO Document_Commune (documentId, communeId) VALUES (%s, %s)", (documentId, communeId))
+
+                # Tabla Organization
+                tipo = 'Revista'
+                query = "SELECT organizationId FROM Organization WHERE organizationName = %s"
                 # Agrega strip() para eliminar espacios en blanco al inicio y final del nombre
-                values = (author,)
+                values = (organization,)
                 cursor.execute(query, values)
                 result = cursor.fetchone()
 
+                # si la organizacion ya existe en la bd
                 if result:
-                    # Si el autor ya existe, no es necesario insertarlo nuevamente
-                    # print(f"El autor '{author}' ya está registrado en la base de datos.")
-                    authorId = result[0]
+                    organizationId = result[0]
+
                 else:
-                    # Si el autor no existe, insertarlo en la tabla Author
-                    query = "INSERT INTO Author (authorName) VALUES (%s)"
+                    query = "INSERT INTO Organization (organizationName, organizationType) VALUES (%s, %s)"
+                    values = (organization, tipo)
                     cursor.execute(query, values)
                     self.connection.commit()
 
                     cursor.execute(
-                        "SELECT authorId FROM Author WHERE authorName = %s", (author,))
-                    authorId = cursor.fetchone()[0]
+                        "SELECT organizationId FROM Organization WHERE organizationName = %s", (organization,))
+                    organizationId = cursor.fetchone()[0]
 
-                # Inserta la relación en Document_Author
+                # Inserta la relación en Document_Organization
                 cursor.execute(
-                    "INSERT INTO Document_Author (documentId, authorId) VALUES (%s, %s)", (documentId, authorId))
+                    "INSERT INTO Document_Organization (documentId, organizationId) VALUES (%s, %s)", (documentId, organizationId))
                 self.connection.commit()
 
-            # Insertar datos en tabla Commune
-            comunas = [str(c).strip() for c in str(row['COMUNAS']).split(
-                ',') if str(c).strip() and str(c).strip().lower() != 'nan']
-            # Verificar si las comunas ya existen en la tabla Commune
-            for comuna in comunas:
+            else:
                 cursor.execute(
-                    "SELECT communeId FROM Commune WHERE name = %s ", (comuna, ))
+                    "SELECT title FROM Document WHERE title = %s", (title,))
                 result = cursor.fetchone()
 
-                if result:
-                    communeId = result[0]
-                    # Si la comuna ya existe, no es necesario agregarla nuevamente
-
-                else:
-                    # Buscar la región correspondiente en el DataFrame commune_region_df
-                    regionMatch = communeRegionData[communeRegionData['Comuna'] == comuna]
-
-                    # match
-                    if not regionMatch.empty:
-                        region = regionMatch['Región'].iloc[0]
-                        latitude = regionMatch['Latitud (Decimal)'].iloc[0]
-                        longitude = regionMatch['Longitud (decimal)'].iloc[0]
-
-                        query = "INSERT INTO Commune (name, region, latitude, longitude) VALUES (%s, %s, %s, %s)"
-                        values = (comuna, region, latitude, longitude)
-                        cursor.execute(query, values)
-                        self.connection.commit()
-
-                        cursor.execute(
-                            "SELECT communeId FROM Commune WHERE name = %s", (comuna,))
-                        communeId = cursor.fetchone()[0]
-
-                    # no match
-                    else:
-                        # Insertar la comuna en la tabla Commune con los campos region, latitude y longitude como NULL
-                        # print(f"No se encontró la región para la comuna '{comuna}'. Se debe realizar el match manualmente.")
-                        query = "INSERT INTO Commune (name, region, latitude, longitude) VALUES (%s, NULL, NULL, NULL)"
-                        values = (comuna,)
-                        cursor.execute(query, values)
-                        self.connection.commit()
-                        cursor.execute(
-                            "SELECT communeId FROM Commune WHERE name = %s", (comuna,))
-                        communeId = cursor.fetchone()[0]
-
-                    # Inserta la relación en Document_Commune
-                cursor.execute(
-                    "INSERT INTO Document_Commune (documentId, communeId) VALUES (%s, %s)", (documentId, communeId))
+                print(f"El título '{title}' existe en la base de datos.")
 
     def insertDocumentData(self, title, pdf_text):
         cursor = self.connection.cursor()
