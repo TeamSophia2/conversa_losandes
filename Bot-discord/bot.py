48a38d3839e49f9ac07a328ca63b0d7b548a8ac0
BothEyes <a.navarrogg@gmail.com>
2023-08-20 13:12:38 -0400

    test llamaindex

--- Bot-discord/bot.py
+++ Bot-discord/bot.py
@@ -10,7 +10,7 @@ from elasticsearch import Elasticsearch
 import openai
 import spacy
 import tiktoken
-from llamaindex import llamaindex
+from llama_index import llamaindex
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 TOKEN = os.environ.get('DISCORD_TOKEN')
