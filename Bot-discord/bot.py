135ed3d47882133a5e72667d605e078383bc91ce
BothEyes <a.navarrogg@gmail.com>
2023-08-23 10:40:24 -0400

    implementacion comando question

--- Bot-discord/bot.py
+++ Bot-discord/bot.py
@@ -286,11 +286,11 @@ class BOT(commands.Cog):
 
             query_engine = new_index.as_query_engine()
 
-            #await ctx.send(question)
+            await ctx.send(question)
 
             response = query_engine.query(question)
-            #await ctx.send(response)
-            print(response) 
+            await ctx.send(response)
+            #print(response) 
 
         else:
             print("NO EXISTE INDEX")
@@ -304,11 +304,11 @@ class BOT(commands.Cog):
             
             query_engine = index.as_query_engine()
 
-            #await ctx.send(question)
+            await ctx.send(question)
 
             response = query_engine.query(question)
-            #await ctx.send(response) 
-            print(response)
+            await ctx.send(response) 
+            #print(response)
             
     
 
