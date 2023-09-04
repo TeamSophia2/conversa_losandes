447973b503a50294470cf2edd5c655302cf0eadb
fernando479 <fernando.ruiz.l@hotmail.cl>
2023-09-04 19:48:07 -0300

    query

--- Bot-discord/bot.py
+++ Bot-discord/bot.py
@@ -227,28 +227,46 @@ class BOT(commands.Cog):
             "query": {
                 "bool": {
                     "must": [],
-                    "filter": []
+                    "filter": [],
+                    "should": []
                 }
             },
             "sort": [{"_score": {"order": "desc"}}],
             "size": 50
         }
 
-        # Si se pasa como argumento, no es obligatorio
-        if searchParams.get("region"):
-            searchBody["query"]["bool"]["must"].append({"match": {"region": searchParams["region"]}})
-        if searchParams.get("categoria"):
-            searchBody["query"]["bool"]["must"].append({"match": {"category": searchParams["categoria"]}})
-        if searchParams.get("comuna"):
-            searchBody["query"]["bool"]["must"].append({"match": {"commune": searchParams["comuna"]}})
-        if searchParams.get("laboratorio"):
-            searchBody["query"]["bool"]["must"].append({"match": {"labTematico": searchParams["laboratorio"]}})
+        # Subconsulta bool para las palabras clave
+        keywordBoolQuery = {
+            "bool": {
+                "should": []
+            }
+        }
+
         if searchParams.get("keywords"):
             keywords = searchParams["keywords"].split(";")
-            keywordQueries = [{"match": {"content": keyword}} for keyword in keywords]
-            # Usar "should" para las palabras clave en "content"
-            searchBody["query"]["bool"]["should"].extend(keywordQueries)
-            searchBody["query"]["bool"]["minimum_should_match"] = 1  # Al menos una palabra clave debe coincidir
+            for keyword in keywords:
+                # Agregar una condición "should" para cada palabra clave en "content"
+                keywordBoolQuery["bool"]["should"].append({"match": {"content": keyword}})
+
+        # Subconsulta bool para las demás condiciones
+        otherBoolQuery = {
+            "bool": {
+                "must": []
+            }
+        }
+
+        if searchParams.get("region"):
+            otherBoolQuery["bool"]["must"].append({"match": {"region": searchParams["region"]}})
+        if searchParams.get("categoria"):
+            otherBoolQuery["bool"]["must"].append({"match": {"category": searchParams["categoria"]}})
+        if searchParams.get("comuna"):
+            otherBoolQuery["bool"]["must"].append({"match": {"commune": searchParams["comuna"]}})
+        if searchParams.get("laboratorio"):
+            otherBoolQuery["bool"]["must"].append({"match": {"labTematico": searchParams["laboratorio"]}})
+
+        # Agregar la subconsulta de palabras clave y la subconsulta de otras condiciones a la consulta principal
+        searchBody["query"]["bool"]["must"].append(keywordBoolQuery)
+        searchBody["query"]["bool"]["must"].append(otherBoolQuery)
 
         # Agregar un filtro para asegurarse de que los documentos tengan el campo "content"
         searchBody["query"]["bool"]["filter"].append({"exists": {"field": "content"}})
@@ -270,7 +288,7 @@ class BOT(commands.Cog):
 
         # Obtener los resultados y formatearlos
         results = response["hits"]["hits"]
-        
+                
         print(f"Se encontraron {len(results)} resultados en Elasticsearch.")
 
         # Divide los resultados en páginas
