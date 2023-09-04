4a47e9837bf1185e05265126659e283f9d99495d
fernando479 <fernando.ruiz.l@hotmail.cl>
2023-09-04 16:48:52 -0300

    test

--- Bot-discord/bot.py
+++ Bot-discord/bot.py
@@ -290,6 +290,8 @@ class BOT(commands.Cog):
             for reaction in reactions:
                 await message.add_reaction(reaction)
 
+            #print(hola)
+
             if total_pages > 1:
                 def check(reaction, user):
                     return user == ctx.author and reaction.message == message
