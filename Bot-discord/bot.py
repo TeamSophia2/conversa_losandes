fef7676751c56c86c866b7567515da04464b3b9b
BothEyes <a.navarrogg@gmail.com>
2023-09-21 18:29:05 -0300

    transcribir

--- Bot-discord/bot.py
+++ Bot-discord/bot.py
@@ -441,7 +441,7 @@ class BOT(commands.Cog):
     @commands.command(name='transcription')
     async def transcription(self,ctx):        
         file = ctx.message.attachments[0]
-        await file.save('../../alvaro/')
+        await file.save('../../alvaro/audio.mp3')
         #await file.save(file.filename)
         transcript = openai.Audio.transcribe("whisper-1", file.filename)
         #await ctx.send(transcript)
